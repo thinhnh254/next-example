82de9fff34dc6ffe4b8b5628c6351caf2ff6a4f6
thinhnh254 <nguyenhungthinh2504@gmail.com>
2024-06-21 15:18:19 +0700

    123

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,8 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug} <p>asdasdasdasdasdasasdasdasdasdasdsad</p></h1>;
+  return <h1>Slug: {params.slug} <p>asdasdasdasdasdasasdasdasdasdasdsad</p>
+  
+  <h2>test</h2>
+  </h1>;
 }
